fefd192d3b4961ce9648034702489b94bea46c74
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2018-03-02 05:22:02 +0100

    LED-Matrix.cpp : for some reason, this doesn't want to loop infinitely

--- source/LED-Matrix.cpp
+++ source/LED-Matrix.cpp
@@ -35,61 +35,76 @@ static unsigned char show[24] = {
   0b00000000, 0b00000000
 };
 
+static unsigned char tell[24] = {
+  0b00000000, 0b00000000,
+  0b00111000, 0b00000000,
+  0b01000100, 0b01100000,
+  0b10010100, 0b10010000,
+  0b00000111, 0b00000000,
+  0b00001000, 0b00100000,
+  0b00110000, 0b10010000,
+  0b00001000, 0b00100000,
+  0b00000111, 0b00000000,
+  0b10010100, 0b10010000,
+  0b01000100, 0b01100000,
+  0b00111000, 0b00000000
+};
+
 static unsigned char com[12] = {0x00, 0x04, 0x08, 0x0C, 0x10, 0x14, 0x18, 0x1C, 0x20, 0x24, 0x28, 0x2C};
 
 void HT1632C_Write(unsigned char Data, unsigned char cnt)      //MCU向HT1632C写数据函数，高位在前/MCU writes the data to ht1632c, and the high position is in front
 {
-    unsigned char i;
-    for(i=0; i<cnt; i++) {
-        HT_WR=0;
-        if(Data&0x80) {
-            HT_DAT=1;
-	} else {
-            HT_DAT=0;
-	}
-        Data<<=1;
-        HT_WR=1;
+  unsigned char i;
+  for(i=0; i<cnt; i++) {
+    HT_WR=0;
+    if(Data&0x80) {
+      HT_DAT=1;
+    } else {
+      HT_DAT=0;
     }
+    Data<<=1;
+    HT_WR=1;
+  }
 }
 
 void HT1632C_Write_CMD(unsigned char cmd)                     //MCU向HT1632c写命令/MCU writes commands to ht1632c
 {
-    HT_CS=0;
-    HT1632C_Write(0x80,3);                                    //ID:100
-    HT1632C_Write(cmd,9);
-    HT_CS=1;
+  HT_CS=0;
+  HT1632C_Write(0x80,3);                                    //ID:100
+  HT1632C_Write(cmd,9);
+  HT_CS=1;
 }
 
 void HT1632C_Write_DAT(unsigned char Addr, unsigned char data[], unsigned char num)
 {
-    unsigned char i;
-    HT_CS=0;
-    HT1632C_Write(0xa0,3);                                    //ID:101
-    HT1632C_Write(Addr<<1,7);
+  unsigned char i;
+  HT_CS=0;
+  HT1632C_Write(0xa0,3);                                    //ID:101
+  HT1632C_Write(Addr<<1,7);
 
-    for(i=0; i<8; i++) {
-        HT_WR=0;
-        if(data[2*(num-1)]&0x80) {
-            HT_DAT=1;
-        } else {
-            HT_DAT=0;
-	}
-        data[2*(num-1)]<<=1;
-        HT_WR=1;
+  for(i=0; i<8; i++) {
+    HT_WR=0;
+    if(data[2*(num-1)]&0x80) {
+      HT_DAT=1;
+    } else {
+      HT_DAT=0;
     }
+    data[2*(num-1)]<<=1;
+    HT_WR=1;
+  }
 
-    for(i=0; i<4; i++) {
-        HT_WR=0;
-        if(data[2*num-1]&0x80) {
-            HT_DAT=1;
-        } else {
-            HT_DAT=0;
-	}
-        data[2*num-1]<<=1;
-        HT_WR=1;
+  for(i=0; i<4; i++) {
+    HT_WR=0;
+    if(data[2*num-1]&0x80) {
+      HT_DAT=1;
+    } else {
+      HT_DAT=0;
     }
+    data[2*num-1]<<=1;
+    HT_WR=1;
+  }
 
-    HT_CS=1;
+  HT_CS=1;
 }
 
 void HT1632C_clr(void)  //清屏函数/Clear function
@@ -106,45 +121,54 @@ void HT1632C_clr(void)  //清屏函数/Clear function
 
 void HT1632C_Init(void)                 //HT1632C初始化函数/HT1632C Init Function
 {
-    HT_CS=1;
-    HT_WR=1;
-    HT_DAT=1;
-    HT1632C_Write_CMD(SYS_DIS);         //关闭HT1632C内部RC时钟/Close the HT1632C internal clock
-    HT1632C_Write_CMD(COM_OPTION);      //选择HT1632C工作模式/Select HT1632C work mode
-    HT1632C_Write_CMD(RC_MASTER_MODE);  //选择内部RC时钟/Select internal clock
-    HT1632C_Write_CMD(SYS_EN);          //打开HT1632C内部RC时钟/Open the HT1632C internal clock
-    HT1632C_Write_CMD(PWM_DUTY);        //PWM亮度设置/Init the PWM Brightness
-    HT1632C_Write_CMD(BLINK_OFF);       //关闭闪烁   /Close blink
-    HT1632C_Write_CMD(LED_ON);          //打开LED显示/ Turn on LED display
+  HT_CS=1;
+  HT_WR=1;
+  HT_DAT=1;
+  HT1632C_Write_CMD(SYS_DIS);         //关闭HT1632C内部RC时钟/Close the HT1632C internal clock
+  HT1632C_Write_CMD(COM_OPTION);      //选择HT1632C工作模式/Select HT1632C work mode
+  HT1632C_Write_CMD(RC_MASTER_MODE);  //选择内部RC时钟/Select internal clock
+  HT1632C_Write_CMD(SYS_EN);          //打开HT1632C内部RC时钟/Open the HT1632C internal clock
+  HT1632C_Write_CMD(PWM_DUTY);        //PWM亮度设置/Init the PWM Brightness
+  HT1632C_Write_CMD(BLINK_OFF);       //关闭闪烁   /Close blink
+  HT1632C_Write_CMD(LED_ON);          //打开LED显示/ Turn on LED display
 }
 
 void HT1632C_Read_DATA(unsigned char Addr)
 {
-    unsigned char i;
-    HT_CS=0;
-    HT1632C_Write(0xc0,3);                                    //ID:101
-    HT1632C_Write(Addr<<1,7);
-    for(i=0; i<12; i++) {
-        HT_RD=0;
-        asm("nop");
-        HT_RD=1;
-    }
-    HT_CS=1;
+  unsigned char i;
+  HT_CS=0;
+  HT1632C_Write(0xc0,3);                                    //ID:101
+  HT1632C_Write(Addr<<1,7);
+  for(i=0; i<12; i++) {
+    HT_RD=0;
+    asm("nop");
+    HT_RD=1;
+  }
+  HT_CS=1;
 }
 
 int main()
 {
-    HT1632C_Init();
-    HT1632C_clr();
-    uBit.init();
-    for(int i=0; i<12; i++) {
-        HT1632C_Write_DAT(com[i],show,i+1);
-    }
-    wait(0.5);
-    while(1) {
-        for(int i=0; i<12; i++) {
-            HT1632C_Read_DATA(com[i]);
-        }
-    }
+  uBit.init();
+ considered_evil:
+  HT1632C_Init();
+  HT1632C_clr();
+  for(int i=0; i<12; i++) {
+    HT1632C_Write_DAT(com[i],show,i+1);
+  }
+  wait(.5);
 
+  HT1632C_Init();
+  HT1632C_clr();
+  for(int i=0; i<12; i++) {
+    HT1632C_Write_DAT(com[i],tell,i+1);
+  }
+  wait(.5);
+
+  for(volatile int t=0; t<32768; ) {
+    for(int i=0; i<12; i++) {
+      HT1632C_Read_DATA(com[i]);
+    }
+  }
+  goto considered_evil;
 }
